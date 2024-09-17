78c31f6317fbf5e14a88bcb790b850cadaf25f6b
auricalgo <91938278+auricalgo@users.noreply.github.com>
2024-09-17 23:47:57 +0530

    0.0.1

---
+++ nandan.py
@@ -0,0 +1,131 @@
+from utils import *
+# import streamlit as st
+
+
+
+
+def nandan_live2(stock_name):
+
+    final_df2 = pd.DataFrame()
+
+    df = fetch_data_from_tru_data(stock_name ,'1 Y')
+    # df.drop(df.tail(1).index,inplace=True)
+    # print(df)
+    df1 = fetch_data_from_tru_data_nandan(stock_name,df['date'].iloc[0],365,'EOD')
+    df_final = pd.concat([df1,df],axis=0).reset_index(drop=True)
+
+    new = calculate_stock_indicators(df.copy(),'atr_14','ATR_14')
+    new = new.drop(['atr_14'],axis=1)
+    new2 = calculate_stock_indicators(new.copy(),'close_50_sma','SMA')
+
+    df_new  = new2[['date','open','close','high','low','atr_14','SMA']]
+   
+    lst = []
+    date_entry = "NA"
+    exit_date = date(2010,2,20)
+    for i in range(df_new.shape[0]-10,df_new.shape[0]):
+        start_date_identify = df_new['date'].iloc[i]
+        if start_date_identify  >= exit_date:
+            supp,resis = get_support_and_resis(df_final,df_new['date'].iloc[i],df_new.iloc[i],stock_name)
+            close_price_sma = df_new['close'].iloc[i]
+            numbers_below_target = [num for num in supp if num < close_price_sma]
+        
+            if len(numbers_below_target) > 0:
+                closest_below_target = max(numbers_below_target)
+                df_new1= df_new[df_new['date'] > start_date_identify].reset_index(drop=True)
+                for j in range(0,df_new1.shape[0]):
+                    if closest_below_target > df_new1['close'].iloc[j]:
+                        support_date_identify = df_new1['date'].iloc[j]
+                        df_new2= df_new1[df_new1['date'] > support_date_identify]
+                        for k in range(0,df_new2.shape[0]):
+                            if ((1.5*df_new2['atr_14'].iloc[k]) < abs(df_new2['close'].iloc[k]-df_new2['open'].iloc[k])) and (df_new2['SMA'].iloc[k] < df_new2['close'].iloc[k]):
+                                entry_price = df_new2['close'].iloc[k]
+                                entry_date = df_new2['date'].iloc[k]
+                                sma_value = df_new2['SMA'].iloc[k]
+                                exit_date = entry_date + timedelta(days=10)
+
+                                dict_list = {'start_date': start_date_identify,'close_price_start':close_price_sma,
+                                            'support':supp,'nearest_support':closest_below_target,
+                                            'date_below_support':support_date_identify,'atr':df_new2['atr_14'].iloc[k],
+                                            'entry':entry_price,'entry_date':entry_date,'Buy/Sell':'Buy','sma_value':sma_value,
+                                            'exit_date':exit_date
+                                            }
+                                # dict_list.update(abc)
+                                lst.append(dict_list)
+                                break
+                        break
+
+    final_df = pd.DataFrame(lst)
+
+    lst = []
+    date_entry = "NA"
+    exit_date = date(2010,2,20)
+    for i in range(df_new.shape[0]-7,df_new.shape[0]):
+        start_date_identify = df_new['date'].iloc[i]
+        if start_date_identify >= exit_date:
+            supp,resis = get_support_and_resis(df_final,df_new['date'].iloc[i],df_new.iloc[i],stock_name)
+            close_price_sma = df_new['close'].iloc[i]
+            numbers_below_target = [num for num in resis if num > close_price_sma]
+
+            if len(numbers_below_target) > 0:
+                closest_above_target = min(numbers_below_target)
+                df_new1= df_new[df_new['date'] > start_date_identify].reset_index(drop=True)
+                for j in range(0,df_new1.shape[0]):
+                    if closest_above_target > df_new1['close'].iloc[j]:
+                        support_date_identify = df_new1['date'].iloc[j]
+                        df_new2= df_new1[df_new1['date'] > support_date_identify]
+                        for k in range(0,df_new2.shape[0]):
+                            if ((1.5*df_new2['atr_14'].iloc[k]) < abs(df_new2['close'].iloc[k]-df_new2['open'].iloc[k])) and (df_new2['SMA'].iloc[k] > df_new2['close'].iloc[k]):
+                                entry_price = df_new2['close'].iloc[k]
+                                entry_date = df_new2['date'].iloc[k]
+                                sma_value = df_new2['SMA'].iloc[k]
+                                exit_date = entry_date + timedelta(days=10)
+
+                                dict_list = {'start_date': start_date_identify,'close_price_start':close_price_sma,
+                                            'support':resis,'nearest_support':closest_above_target,
+                                            'date_below_support':support_date_identify,'atr':df_new2['atr_14'].iloc[k],
+                                            'entry':entry_price,'entry_date':entry_date,'Buy/Sell':'Sell',
+                                            'sma_value':sma_value,'exit_date':exit_date
+                                            }
+                                # dict_list.update(abc)
+                                lst.append(dict_list)
+                                break
+                        break
+
+    final_df1 = pd.DataFrame(lst)
+    
+    final_df2 = pd.concat([final_df1,final_df]).reset_index(drop=True)
+    
+    return final_df2
+
+
+def get_support_and_resis(df,date_for_s_r,x,stock_name):
+
+    df2 = df[(df['date']<date_for_s_r) & (df['date']>=date_for_s_r-timedelta(days = 365))]
+    df2 = df2.reset_index(drop=True)
+    if date_for_s_r-timedelta(days = 365) in df['date']:
+        df1 = df2.copy()
+    else:
+        df3 = pd.DataFrame([x]).reset_index(drop=True)
+        df1 = fetch_data_from_tru_data_nandan(stock_name,df3['date'].iloc[0],365,'EOD')
+    supp,resis = s_and_r(df1)
+
+    return supp,resis
+
+
+def final_run():
+    df9 = pd.DataFrame()
+    for i in NIFTY100:
+        # start_time = time.time()
+        df = pd.DataFrame()
+        df = nandan_live2(i)
+        if df.shape[0]:
+            df['Company'] = i
+            df9 = pd.concat([df9,df]).reset_index(drop=True)
+        # end = time.time() - start_time
+    # df9.to_csv('old_live.csv')
+    if df9.shape[0]:
+        df9['date_of_run'] = datetime.datetime.today().strftime('%Y-%m-%d')
+        df9['time_of_run'] = datetime.datetime.today().strftime('%H:%M:%S')
+    df9.to_csv('live.csv', mode='a', header=False, index=False)
+    return df9
---
+++ utils.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import stockstats
+import numpy as np
+from truedata_ws.websocket.TD import TD
+from datetime import date,datetime,time,timedelta
+from dateutil.relativedelta import relativedelta,TH,FR,WE
+
+
+td_obj = TD('tdws119', 'mridul@119',live_port=None)
+
+def fetch_data_from_tru_data(symbol,time='5 Y'):
+    ''' Fetching stock data from true data and return a Dataframe'''
+
+    # td_obj = TD('tdws119', 'mridul@119',live_port=8082)
+    pyr = td_obj.get_historic_data(symbol, duration=time, bar_size='EOD')
+    df = pd.DataFrame.from_dict(pyr)
+    df = df.rename(columns={"time":"date","c":"close","o":"open","l":"low","h":"high"})
+    df['date'] = pd.to_datetime(df['date'])
+    # td_obj.disconnect()
+
+    return df
+
+
+def fetch_data_from_tru_data_nandan(symbol,date1,day,bar_size):
+    ''' Fetching stock data from true data and return a Dataframe'''
+
+    pyr = td_obj.get_historic_data(symbol,start_time=date1-relativedelta(days=day),end_time=date1,bar_size=bar_size)
+    df = pd.DataFrame(pyr)
+    df = df.rename(columns={"time":"date","c":"close","o":"open","l":"low","h":"high"})
+    df['date'] = pd.to_datetime(df['date'])
+
+    return df
+
+
+def calculate_stock_indicators(stockprices,type,columnName):
+    ''' Stock Indicators '''
+
+    stock = stockstats.StockDataFrame.retype(stockprices)
+    stock = stock[type]
+    stock = stock.reset_index()
+    stock =stock.rename(columns={type:columnName})
+    new1 = pd.merge(stockprices, stock, left_on='date',right_on='date')
+
+    return new1
+
+def isSupport(df,i):
+  support = df['low'][i] < df['low'][i-1]  and df['low'][i] < df['low'][i+1] and df['low'][i+1] < df['low'][i+2] and df['low'][i-1] < df['low'][i-2]
+  return support
+
+def isResistance(df,i):
+  resistance = df['high'][i] > df['high'][i-1]  and df['high'][i] > df['high'][i+1] and df['high'][i+1] > df['high'][i+2] and df['high'][i-1] > df['high'][i-2]
+  return resistance
+
+def isFarFromLevel(l,levels,s):
+   return np.sum([abs(l-x) < s  for x in levels]) == 0
+
+def s_and_r(df):
+    s =  np.mean(df['high'] - df['low'])
+    support = []
+    resistance = []
+    levels = []
+    for i in range(2,df.shape[0]-2):
+        if isSupport(df,i):
+            l = df['low'][i]
+            if isFarFromLevel(l,levels,s):
+                levels.append(l)
+                support.append(round(l,2))
+        elif isResistance(df,i):
+            l = df['high'][i]
+            if isFarFromLevel(l,levels,s):
+                levels.append(l)
+                resistance.append(round(l,2))
+
+    return support,resistance
+
+NIFTY100 = [
+'ABB',
+'ADANIENSOL',
+'ADANIENT',
+'ADANIGREEN',
+'ADANIPORTS',
+'ADANIPOWER',
+'ATGL',
+'AMBUJACEM',
+'APOLLOHOSP',
+'ASIANPAINT',
+'DMART',
+'AXISBANK',
+'BAJAJ-AUTO',
+'BAJFINANCE',
+'BAJAJFINSV',
+'BAJAJHLDNG',
+'BANKBARODA',
+'BERGEPAINT',
+'BEL',
+'BPCL',
+'BHARTIARTL',
+'BOSCHLTD',
+'BRITANNIA',
+'CANBK',
+'CHOLAFIN',
+'CIPLA',
+'COALINDIA',
+'COLPAL',
+'DLF',
+'DABUR',
+'DIVISLAB',
+'DRREDDY',
+'EICHERMOT',
+'GAIL',
+'GODREJCP',
+'GRASIM',
+'HCLTECH',
+'HDFCBANK',
+'HDFCLIFE',
+'HAVELLS',
+'HEROMOTOCO',
+'HINDALCO',
+'HAL',
+'HINDUNILVR',
+'ICICIBANK',
+'ICICIGI',
+'ICICIPRULI',
+'ITC',
+'IOC',
+'IRCTC',
+'IRFC',
+'INDUSINDBK',
+'NAUKRI',
+'INFY',
+'INDIGO',
+'JSWSTEEL',
+'JINDALSTEL',
+'JIOFIN',
+'KOTAKBANK',
+'LTIM',
+'LT',
+'LICI',
+'M&M',
+'MARICO',
+'MARUTI',
+'NTPC',
+'NESTLEIND',
+'ONGC',
+'PIDILITIND',
+'PFC',
+'POWERGRID',
+'PNB',
+'RECLTD',
+'RELIANCE',
+'SBICARD',
+'SBILIFE',
+'SRF',
+'MOTHERSON',
+'SHREECEM',
+'SHRIRAMFIN',
+'SIEMENS',
+'SBIN',
+'SUNPHARMA',
+'TVSMOTOR',
+'TCS',
+'TATACONSUM',
+'TATAMTRDVR',
+'TATAMOTORS',
+'TATAPOWER',
+'TATASTEEL',
+'TECHM',
+'TITAN',
+'TORNTPHARM',
+'TRENT',
+'ULTRACEMCO',
+'UNITDSPR',
+'VBL',
+'VEDL',
+'WIPRO',
+'ZOMATO',
+'ZYDUSLIFE'
+]
